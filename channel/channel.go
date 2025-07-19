028c1c57e37f8cea49ae1062df0be859e7d7ac4d
kittipat <ballkittipat272@hotmail.com>
2025-07-19 22:45:28 +0700

    feat: learn goroutine

---
+++ channel/channel.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func process1(ch chan string, data string) {
+	fmt.Println("sending:", data)
+	ch <- data
+}
+
+func main() {
+	fmt.Println("=== main started ===")
+
+	ch := make(chan string, 1)
+	go process1(ch, "data1")
+
+	msg := <-ch
+	fmt.Println("received from channel:", msg)
+
+	time.Sleep(5 * time.Second)
+	fmt.Println("=== main done ===")
+}
